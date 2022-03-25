4fbc0085772f3d4455acdd0b400489fdf5382b95
Victoria <victoriarailean1998@gmail.com>
2022-03-25 21:40:39 +0200

    added order entity

---
+++ src/main/java/com/epam/onlinestore/entities/Order.java
@@ -0,0 +1,33 @@
+package com.epam.onlinestore.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "order_seq")
+    @SequenceGenerator(name = "order_seq")
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @OneToMany
+    @JoinTable(
+            name = "order_product",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id"))
+    Set<Product> products;
+
+    @ManyToOne
+    User user;
+}
