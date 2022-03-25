62d4152b6050d7192daff6149e94c69c3cb5408c
Victoria <victoriarailean1998@gmail.com>
2022-03-25 21:39:59 +0200

    added product entity

---
+++ src/main/java/com/epam/onlinestore/entities/Product.java
@@ -0,0 +1,32 @@
+package com.epam.onlinestore.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String name;
+    private String description;
+    private String price;
+    private String imageName;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+}
