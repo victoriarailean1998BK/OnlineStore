fcb9ac1ce6328b8c1c579d983d2dae5218bdc431
Victoria <victoriarailean1998@gmail.com>
2022-03-25 21:40:28 +0200

    added product to category relation

--- src/main/java/com/epam/onlinestore/entities/Product.java
+++ src/main/java/com/epam/onlinestore/entities/Product.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.Set;
 
 @Entity
 @Getter
@@ -28,5 +27,4 @@ public class Product {
     @ManyToOne
     @JoinColumn(name = "category_id")
     private Category category;
-
 }
