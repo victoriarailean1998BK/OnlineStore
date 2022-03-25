8567bdeea385386046612e2b3d318ca115ee09fc
Victoria <victoriarailean1998@gmail.com>
2022-03-25 21:40:12 +0200

    added category entity

---
+++ src/main/java/com/epam/onlinestore/entities/Category.java
@@ -0,0 +1,23 @@
+package com.epam.onlinestore.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String name;
+}
